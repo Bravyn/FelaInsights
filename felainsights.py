38e268dded9457aa11276b77a6398100aa4c2eed
Ian Bravyn <ianbravynsa@gmail.com>
2023-07-02 23:37:22 +0300

    now displaying google trends csv data

---
+++ felainsights.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from search_trends.display_csv import show_csv
+
+t1, t2 = st.tabs(["Fella Trends", "Lyric Sentiments"])
+
+with t1:
+    st.write("Fella Trends")
+    show_csv()
+with t2:
+    st.write("Lyric Sentiments")
---
+++ search_trends/display_csv.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+
+def load_csv(filename = './data/geoMap.csv'):
+    return pd.read_csv(filename)
+
+csv = load_csv()
+
+def show_csv(csv = csv):
+    
+    st.write(csv)
+
+
