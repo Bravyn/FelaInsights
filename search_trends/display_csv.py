73b76a27f6c2037185069d6f7d661dd8c926623e
Ian Bravyn <ianbravynsa@gmail.com>
2023-07-03 04:56:12 +0300

    implemented a matplotlib plot

--- search_trends/display_csv.py
+++ search_trends/display_csv.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def load_csv(filename = './data/geoMap.csv'):
@@ -8,7 +9,17 @@ def load_csv(filename = './data/geoMap.csv'):
 csv = load_csv()
 
 def show_csv(csv = csv):
+    states = csv.State
+    values = csv.Search
+
+    fig, ax = plt.subplots()
+    ax.bar(states, values)
+    ax.set_xlabel("State")
+    ax.set_ylabel("Search Volume")
+    plt.xticks(rotation = 'vertical')
+    ax.set_title("Fela Kuti Google Search Trend")
+    st.pyplot(fig)
     st.write(csv)
-    st.line_chart(csv.Search.head(5))
+    st.line_chart(csv.Search.head(23))
 
 
